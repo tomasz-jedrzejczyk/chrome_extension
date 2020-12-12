ba4f56c51b44200029aedfd375bd4b46506b8966
TomaszJedrzejczyk <tomaszjedrz@gmail.com>
2020-12-12 21:02:16 +0100

    main
    first version of extension 1.0

---
+++ pages/popup/scripts/background.js
@@ -0,0 +1,28 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds; 
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+
+        if (timer == 0) {
+            alert("message");
+        };
+
+    }, 1000);
+
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
---
+++ scripts/background.js
@@ -0,0 +1,5 @@
+chrome.runtime.onInstalled.addListener((details) => {
+    console.log('previousVersion', details.previousVersion)
+});
+console.log('Event Page for Page Action');
+
